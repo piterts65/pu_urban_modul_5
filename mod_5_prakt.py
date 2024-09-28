e34914b9fbb0f8d58bbc5e7a3bae2f3e381c3ae4
user <piterts65@gmail.com>
2024-09-28 13:48:28 +0500

    ??

---
+++ mod_5_prakt.py
@@ -0,0 +1,21 @@
+class Database:
+    def __init__(self):
+        self.data = {}
+
+    def add_user(self, username, password):
+        self.data[username] = password
+
+class User:
+    """
+    класс пользователя, атрибуты: логин, пароль
+    """
+    def __int__(self, username, password, password_confirm):
+        self.username = username
+        if password == password_confirm:
+            self.password = password
+
+
+if __name__ == '__main__':
+    database = Database
+    user = User(input("Login: "), input("Pass: "), input("Con pass: "))
+    database.add_user(user.username, user.password)
