f773b6467e1ad3bc3c88486df0722540f172df74
user <piterts65@gmail.com>
2024-09-29 10:50:22 +0500

    3

--- mod_5_prakt.py
+++ mod_5_prakt.py
@@ -18,5 +18,23 @@ class User:
 
 if __name__ == '__main__':
     database = Database()
-    user = User(input("Login: "), input("Pass: "), input("Con pass: "))
-    database.add_user(user.username, user.password)
+    while True:
+        choise = input('Приветствую! Выберите действие:\n 1 - Вход \n 2 -Регистрация \n ')
+        if choise == '1':
+            login = input("Login: ")
+            password = input("Pass: ")
+            if login in database.data:
+                if password == database.data[login]:
+                    print(f'Вход выполнен {login}')
+                    break
+                if password != database.data[login]:
+                    print('Неверный пароль')
+            else:
+                print('Eror')
+        if choise == '2':
+            user = User(input("Login: "), password := input("Pass: "),
+                        password_c := input("Con pass: "))
+            if password != password_c:
+                continue
+            database.add_user(user.username, user.password)
+        print(database.data)
