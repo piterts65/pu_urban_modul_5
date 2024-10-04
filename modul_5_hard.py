27fd9492a6d8b2c55fb50c2b8553ddf1303ca656
user <piterts65@gmail.com>
2024-10-04 14:40:13 +0500

    ??+?

--- modul_5_hard.py
+++ modul_5_hard.py
@@ -19,7 +19,7 @@ class User:
 
 class Video:
     
-    def __init__(self, title, duration, adult_mode):
+    def __init__(self, title, duration, adult_mode=False):
         self.title = title
         self.duration = duration
         self.adult_mode = adult_mode
@@ -30,10 +30,6 @@ class Video:
 
     def __repr__(self):
         return self.title
-
-    def __bool__(self, adult_mode):
-        return False
-
 
 class UrTube:
 
@@ -90,29 +86,30 @@ class UrTube:
                 break
 
 ur = UrTube()
-v1 = Video('Лучший язык программирования 2024 года', 200,adult_mode=False)
-v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+v1 = Video('Лучший язык программирования 2024 года', 200)
+v2 = Video('Для чего девушкам парень программист?', 10)
 
 # Добавление видео
 ur.add(v1, v2)
 
 # Проверка поиска
-print(ur.get_videos('лучший'))
-print(ur.get_videos('ПРОГ'))
+#print(ur.get_videos('лучший'))
+#print(ur.get_videos('ПРОГ'))
 
 # Проверка на вход пользователя и возрастное ограничение
-ur.watch_video('Для чего девушкам парень программист?')
+#ur.watch_video('Для чего девушкам парень программист?')
 ur.register('vasya_pupkin', 'lolkekcheburek', 13)
-ur.watch_video('Для чего девушкам парень программист?')
-ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
-ur.watch_video('Для чего девушкам парень программист?')
+print(ur.current_user)
+ur.watch_video('Лучший язык программирования 2024 года')
+#ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+#ur.watch_video('Для чего девушкам парень программист?')
 
 # Проверка входа в другой аккаунт
-ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
-print(ur.current_user)
+#ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+#print(ur.current_user)
 
 # Попытка воспроизведения несуществующего видео
-ur.watch_video('Лучший язык программирования 2024 года!')
+#ur.watch_video('Лучший язык программирования 2024 года!')
 
 
 
