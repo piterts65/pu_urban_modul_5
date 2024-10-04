f1ec961488fc0e360b0c3d304a36409a56497a1e
user <piterts65@gmail.com>
2024-10-04 15:40:29 +0500

    ??+?

--- modul_5_hard.py
+++ modul_5_hard.py
@@ -74,42 +74,42 @@ class UrTube:
             return
         for video in self.videos:
             if title == video.title:
-                if video.adult_mode and self.current_user.age >= 18:
+                if self.current_user.age <= 18 and video.adult_mode is True:
+                    print('Вам нет 18 лет, пожалуйста покиньте страницу')
+                    break
+                else:
                     while video.time_now < video.duration:
                         video.time_now += 1
                         print(video.time_now, end=' ')
                         time.sleep(1)
                     video.time_now = 0
                     print('Конец видео')
-                else:
-                    print('Вам нет 18 лет, пожалуйста покиньте страницу')
-                break
+                
 
 ur = UrTube()
 v1 = Video('Лучший язык программирования 2024 года', 200)
-v2 = Video('Для чего девушкам парень программист?', 10)
+v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
 
 # Добавление видео
 ur.add(v1, v2)
 
 # Проверка поиска
-#print(ur.get_videos('лучший'))
-#print(ur.get_videos('ПРОГ'))
+print(ur.get_videos('лучший'))
+print(ur.get_videos('ПРОГ'))
 
 # Проверка на вход пользователя и возрастное ограничение
 #ur.watch_video('Для чего девушкам парень программист?')
 ur.register('vasya_pupkin', 'lolkekcheburek', 13)
-print(ur.current_user)
-ur.watch_video('Лучший язык программирования 2024 года')
-#ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
-#ur.watch_video('Для чего девушкам парень программист?')
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+ur.watch_video('Для чего девушкам парень программист?')
 
 # Проверка входа в другой аккаунт
-#ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
-#print(ur.current_user)
+ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+print(ur.current_user)
 
 # Попытка воспроизведения несуществующего видео
-#ur.watch_video('Лучший язык программирования 2024 года!')
+ur.watch_video('Лучший язык программирования 2024 года!')
 
 
 
