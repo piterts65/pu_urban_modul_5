e64a51c2241ee27643da1aafd5b6ee38803bb79b
user <piterts65@gmail.com>
2024-10-04 15:41:17 +0500

    end

--- modul_5_2.py
+++ modul_5_2.py
@@ -11,11 +11,21 @@ class House:
                 print('"Такого этажа не существует"')
                 break
 
-    def __str__(self):
-       return (f'"Название: {self.name}, кол-во этажей: {self.number_of_floors}"')
+    h1 = House('ЖК Эльбрус', 10)
+    h2 = House('ЖК Акация', 20)
+
+    print(h1)
+    print(h2)
 
     def __len__(self):
         return self.number_of_floors
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.
+
+    def __add__(self, value):
+        return self.number_of_floors ==self.number_of_floors + value
+
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
@@ -23,9 +33,18 @@ h2 = House('ЖК Акация', 20)
 #h2.go_to(10)
 
 # __str__
+#print(h1)
+#print(h2)
+
+# __len__
+#print(len(h1))
+#print(len(h2))
+
 print(h1)
 print(h2)
 
-# __len__
-print(len(h1))
-print(len(h2))
+print(h1 == h2) # __eq__
+
+
+def __add__(self, value):
+    return self.number_of_floors == self.number_of_floors + value
